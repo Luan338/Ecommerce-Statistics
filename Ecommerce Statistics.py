f5d5215fec420d8d24fb1edc6411bd6f6b10fbc6
Luan Eric Silva <75584515+Luan338@users.noreply.github.com>
2025-08-27 13:53:11 -0300

    New Updates
    New updates in Python file

--- Ecommerce Statistics.py
+++ Ecommerce Statistics.py
@@ -1,1 +1,28 @@
-print('Olá mundo!')
+# Library Imports: pandas, numpy, matplotlib, seaborn
+import pandas as pd;
+import numpy as np;
+import matplotlib.pyplot as plt;
+import seaborn as sns;
+
+# Import Data Source: Ecommerce Statistics.csv
+df = pd.read_csv('ecommerce_estatistica.csv');
+
+# 01) Data Reading and Cleaning
+print('Initial view of the data (head):');
+print(df.head());
+print('\n');
+
+print('Dataframe Information (info):');
+print(df.info());
+print('\n');
+
+print('Count missing values by column:');
+print(df.isnull().sum());
+print('\n');
+
+print(f"\nDataFrame {df.shape[0]} lines and {df.shape[1]} columns.")
+# ////////////////////////////////////////////////////////////////////////////////////////////////
+
+# 02) Detailed Analysis
+
+
